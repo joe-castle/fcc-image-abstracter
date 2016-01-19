360ab2438286e0888d3c9f4683e32f2caa15ad14
Joe Smith <joesmith0488@gmail.com>
2016-01-19 15:48:37 +0000

    tests to imagesearch api

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,17 @@ const path = require('path');
 
 const app = express();
 
-
+app.get('/api/imagesearch/:searchString', (req, res) => {
+  if (req.params.searchString === 'thiswontwork') {
+    res.json({error: 'No images found'});
+  } else {
+    res.json([{
+      url: '',
+      snippet: '',
+      thumbnail: '',
+      context: '',
+    }]);
+  }
+});
 
 module.exports = app;
--- test/routes-test.js
+++ test/routes-test.js
@@ -17,5 +17,33 @@ describe('Express Routes', () => {
         .get('/api/imagesearch/lolcats')
         .expect('Content-Type', /json/, done);
     });
+    it('Returns a list of images if the search returns results', (done) => {
+      request(app)
+        .get('/api/imagesearch/lolcats')
+        .expect((res) => {
+          expect(res.body).to.have.length.above(0);
+        })
+        .end(done);
+    })
+    it('Returns error if the search returns no results', (done) => {
+      const expectedResponse = {
+        error: 'No images found'
+      };
+      request(app)
+        .get('/api/imagesearch/thiswontwork')
+        .expect((res) => {
+          expect(res.body).to.deep.equal(expectedResponse);
+        })
+        .end(done);
+    })
+    it('Each image should contain key/value pairs', (done) => {
+      const expectedResponse = ['url', 'snippet', 'thumbnail', 'context'];
+      request(app)
+        .get('/api/imagesearch/lolcats')
+        .expect((res) => {
+          expect(res.body[0]).to.have.all.keys(expectedResponse);
+        })
+        .end(done);
+    });
   });
 });
